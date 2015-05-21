758fc9d041513ca7d4971b59db0ff9457c17b935
Tristan Rubadeau <tristan@rubadeau.com>
2015-05-20 22:12:44 -0600

    signout spec

---
+++ spec/features/signout_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+  feature 'Sign Out' do
+
+    before :each do
+      User.destroy_all
+    end
+
+    scenario 'allows a user to sign out' do
+
+      user = User.create!(
+      :user_user_name, with: 'Testy',
+      :user_email, with: 'testyMcTesterson@example.com',
+      :user_password, with: 'password'
+      :user_password_confirmation, with: 'password')
+
+      visit sign_in_path
+
+      fill_in :user_email, with: 'testyMcTesterson@example.com'
+      fill_in :user_password, with: 'password'
+      click_button 'Sign In'
+
+      expect(current_path).to eq competitions_path
+      expect(page).to have_content 'Such workout, much fitness'
+
+      click_on 'Sign Out'
+
+      expect(page).to have_content 'Sign In'
+      expect(page).to have_content 'You have successfully logged out'
+    end
+end
